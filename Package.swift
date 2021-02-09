9081caf09ba36f76504a52d91bf88319a516d768
Siarhei Fedartsou <siarhei.fedartsou@mapbox.com>
2021-02-09 13:37:51 +0300

    Use exact MapboxCommon version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", from: mapboxCommonVersion),
+        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", .exact(mapboxCommonVersion)),
     ],
     targets: [
         registry.mapboxNavigationNativeTarget(version: version, checksum: checksum),
