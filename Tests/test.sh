792979676703e7c7c17923a8681ee8e3bd742680
Siarhei Fedartsou <siarhei.fedartsou@mapbox.com>
2021-11-26 10:30:48 +0100

    Do not suppress output of tests to be able to debug it later

--- Tests/test.sh
+++ Tests/test.sh
@@ -5,11 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="${DIR}/.."
 
 echo "Testing SPM..."
-${ROOT_DIR}/Tests/test_spm.sh >/dev/null
+${ROOT_DIR}/Tests/test_spm.sh
 
 echo "Testing Carthage..."
-${ROOT_DIR}/Tests/test_carthage.sh >/dev/null
+${ROOT_DIR}/Tests/test_carthage.sh
 
 echo "Testing CocoaPods..."
-${ROOT_DIR}/Tests/test_cocoapods.sh >/dev/null
+${ROOT_DIR}/Tests/test_cocoapods.sh
 
