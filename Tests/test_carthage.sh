f09a4e71acc4d3f8e16441f53e1ae7d68f4eee37
Dzianis Papou <dzianis.papou@mapbox.com>
2023-10-13 14:50:56 +0200

    [NN-1124] Migrate CircleCI MacOS runners from Intel to arm64 (#16)
    Co-authored-by: Alex Azarov <alex@azarov.by>

--- Tests/test_carthage.sh
+++ Tests/test_carthage.sh
@@ -6,7 +6,7 @@ ROOT_DIR="${DIR}/.."
 pushd "${ROOT_DIR}/Tests/Carthage"
 
 xcodegen generate
-carthage update --platform iOS --use-netrc
+carthage update --platform iOS --use-netrc --use-xcframeworks
 xcodebuild -project CarthageTest.xcodeproj -scheme CarthageTest -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.5' build
 
 popd
